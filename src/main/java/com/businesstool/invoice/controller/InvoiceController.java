1cdb504afa4ab833c3897d26f099a51f546f2d26
toyin <myworldco@gmail.com>
2024-06-17 10:58:12 +0200

    FIX Controller Security Problem

--- src/main/java/com/businesstool/invoice/controller/InvoiceController.java
+++ src/main/java/com/businesstool/invoice/controller/InvoiceController.java
@@ -3,6 +3,7 @@ package com.businesstool.invoice.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,33 +11,42 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.businesstool.invoice.dtos.InvoiceDto;
 import com.businesstool.invoice.service.InvoiceService;
 
+import lombok.RequiredArgsConstructor;
+
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/invoices")
 public class InvoiceController {
     @Autowired
     private InvoiceService invoiceService;
 
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     public List<InvoiceDto> getAllInvoices() {
         return invoiceService.getAllInvoices();
+       //return "Work in Progress try again later ";
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public InvoiceDto createInvoice(@RequestBody InvoiceDto invoiceDTO) {
         return invoiceService.createInvoice(invoiceDTO);
     }
 
     @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public InvoiceDto updateInvoice(@PathVariable Long id, @RequestBody InvoiceDto invoiceDTO) {
         return invoiceService.updateInvoice(id, invoiceDTO);
     }
 
     @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public void deleteInvoice(@PathVariable Long id) {
         invoiceService.deleteInvoice(id);
     }
